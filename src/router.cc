a40872ecfe09943d69c365928c03d0c149e94871
chn0318 <chn@berkeley.edu>
2024-06-11 23:01:45 +0000

    finish check6

--- src/router.cc
+++ src/router.cc
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+static uint32_t setLeadingBits(uint8_t n) {
+    if (n <= 0) {
+        return 0;
+    }
+    if (n >= 32) {
+        return ~0U;
+    }
+    return (~0U << (32 - n));
+}
 // route_prefix: The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
 // prefix_length: For this route to be applicable, how many high-order (most-significant) bits of
 //    the route_prefix will need to match the corresponding bits of the datagram's destination address?
@@ -20,11 +29,54 @@ void Router::add_route( const uint32_t route_prefix,
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
 
-  // Your code here.
+  element item(route_prefix, prefix_length, next_hop, interface_num);
+  router_table.push_back(item);
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  // Your code here.
+  for (size_t i = 0; i < _interfaces.size(); i++)
+  {
+    while (!interface(i)->datagrams_received().empty())
+    {
+      InternetDatagram msg = interface(i)->datagrams_received().front();
+      interface(i)->datagrams_received().pop();
+      // get the dst ip addr
+      uint32_t dst_ip_addr = msg.header.dst;
+      uint32_t max_length = 0;
+      bool flag = false;
+      uint32_t forward_addr = dst_ip_addr;
+      size_t interface_num;
+      //traverse router_table
+      for (auto item : router_table)
+      {
+        uint32_t mask = setLeadingBits(item.prefix_length);
+        if ((mask & item.route_prefix) == (mask & dst_ip_addr))
+        {
+          
+          if (max_length < item.prefix_length || flag == false)
+          {
+            max_length = item.prefix_length;
+            forward_addr = item.next_hop.has_value()?item.next_hop.value().ipv4_numeric():dst_ip_addr;
+            interface_num = item.interface_num;
+          }
+          flag = true;
+        }  
+      }
+      //drop package if router table do not has match item or ttl <= 0
+      if (!flag)
+      {
+        continue;
+      }
+      if (msg.header.ttl <= 1)
+      {
+        continue;
+      }
+      msg.header.ttl--;
+      interface(interface_num)->send_datagram(msg, Address::from_ipv4_numeric(forward_addr));
+    }
+    
+  }
+  
 }
--- src/router.hh
+++ src/router.hh
@@ -5,11 +5,23 @@
 
 #include "exception.hh"
 #include "network_interface.hh"
+class element
+{
+  public:
+    uint32_t route_prefix;
+    uint8_t prefix_length;
+    std::optional<Address> next_hop;
+    size_t interface_num;
 
+    element(uint32_t _route_prefix, uint8_t _prefix_length, std::optional<Address> _next_hop, size_t _interface_num)
+      :route_prefix(_route_prefix), prefix_length(_prefix_length),next_hop(_next_hop),interface_num(_interface_num){}
+};
+  
 // \brief A router that has multiple network interfaces and
 // performs longest-prefix-match routing between them.
 class Router
 {
+
 public:
   // Add an interface to the router
   // \param[in] interface an already-constructed network interface
@@ -35,4 +47,5 @@ public:
 private:
   // The router's collection of network interfaces
   std::vector<std::shared_ptr<NetworkInterface>> _interfaces {};
+  std::vector<element> router_table{};
 };
